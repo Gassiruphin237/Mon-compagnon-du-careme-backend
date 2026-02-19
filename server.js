d1ffef5aed9fccda4d22441c2dd8cd547a751dd1
Gassiruphin237 <gassiruphin@gmail.com>
2026-02-19 17:51:39 +0100

    update backend version1.0.0

--- server.js
+++ server.js
@@ -12,10 +12,17 @@ const app = express();
 
 // Middleware
 // app.use(cors());
-app.use(cors({
-  origin: ["http://192.168.50.171:5173", "http://localhost:5173"], 
-  credentials: true
-}));
+// app.use(cors({
+//   origin: ["http://192.168.50.171:5173", "http://localhost:5173"], 
+//   credentials: true
+// }));
+
+const corsOptions = {
+  origin: 'https://mon-compagnon-du-careme.vercel.app', 
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, 
+  optionsSuccessStatus: 200
+};
 app.use(express.json());
   
 // Routes
@@ -30,7 +37,7 @@ const PORT = process.env.PORT || 7500;
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
-    console.log("✅ MongoDB connecté");
+    console.log(" MongoDB connecté");
     app.listen(PORT, "0.0.0.0", () => {
       console.log(`Serveur lancé sur : http://localhost:${PORT}`);
       console.log(` Accès réseau local : http://YOUR_IP_ADDRESS:${PORT}`);
